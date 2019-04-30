00af006fe380675ae8ca7c421d672614272f3888
Morgan Dorfman <mdorfman21@gmail.com>
2019-04-30 14:23:34 -0400

    added more changes to the app

--- app.js
+++ app.js
@@ -6,12 +6,8 @@ let bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-let exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
 require("./controllers/api_controller")(app);
+require("./controllers/html_controller")(app);
 
 app.listen(PORT, () => {
   console.log(`listening on http://localhost:${PORT}`);
